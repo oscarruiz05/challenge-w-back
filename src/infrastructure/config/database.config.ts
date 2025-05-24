4cbae4fd4d3cfa2590af31801ac8ea10078bd995
Oscar Ruiz <oscarruiz2614@gmail.com>
2025-05-23 22:16:04 -0500

    feat: :sparkles: configuracion de docker y dabse de datos

---
+++ src/infrastructure/config/database.config.ts
@@ -0,0 +1,23 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+  synchronize: process.env.NODE_ENV === 'development',
+  logging: process.env.NODE_ENV === 'development',
+  migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
+  migrationsRun: false,
+};
+
+export const dataSourceOptions = {
+  ...databaseConfig,
+  migrationsTableName: 'migrations',
+};
